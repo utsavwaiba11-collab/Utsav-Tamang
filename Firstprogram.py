243d59fdb5ab2703e6e107d59fd33eef5beb938a
utsavwaiba11-collab <utsav.waiba11@gmail.com>
2025-08-20 20:06:53 +0545

    Update Firstprogram.py

--- Firstprogram.py
+++ Firstprogram.py
@@ -3,11 +3,14 @@ num2 = float(input("Enter second number"))
 
 num1 = int(num1)
 num2 = int(num2)
-
 print("\n--- Simple Calculation Results \n")
 print("sum", num1 + num2)
+print("difference", num1 - num2)
 print("multiplication", num1 * num2)
 print("division", num1 / num2)
 print("modulo", num1 % num2)
 print("power",num1 ** num2)
+print("int_division",num1 // num2)
+
+
 

61346e103b29523d02d8125e92f14bd11fdde224
utsavwaiba11-collab <utsav.waiba11@gmail.com>
2025-08-20 19:56:24 +0545

    Update Firstprogram.py

--- Firstprogram.py
+++ Firstprogram.py
@@ -11,18 +11,3 @@ print("division", num1 / num2)
 print("modulo", num1 % num2)
 print("power",num1 ** num2)
 
-print("---\n Simple Form")
-
-
-
-      
-name = input("\nEnter your name: ")
-age = int(input("Enter your age: "))
-height = float(input("Enter your height in feet: "))
-country = input("Enter your country: ")
-
-# Processing
-name_upper = name.upper()
-country_upper = country.upper()
-nickname = (name[:2] + name[-2:]).upper()
-height_rounded = round(height, 2)
